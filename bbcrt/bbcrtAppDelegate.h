a9036d439cceb156da8e860a38400379deed3e6a
momohach <hachfi.mohamed.amin@gmail.com>
2012-04-12 19:22:37 +0100

    cm1

---
+++ bbcrt/bbcrtAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  bbcrtAppDelegate.h
+//  bbcrt
+//
+//  Created by Snow Leopard User on 12/04/2012.
+//  Copyright 2012 momo_dingua. All rights reserved.
+// test
+
+#import <UIKit/UIKit.h>
+
+@interface bbcrtAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ bbcrt/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bbcrt
+//
+//  Created by Snow Leopard User on 12/04/2012.
+//  Copyright 2012 momo_dingua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
