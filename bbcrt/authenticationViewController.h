4862e157f6120b826f8788b3093bbcb40905dfc4
repinteractive <hachfi.mohamed.amine@gmail.com>
2012-04-17 12:39:51 +0100

    images, home view, signIn View

---
+++ bbcrt/authenticationViewController.h
@@ -0,0 +1,13 @@
+//
+//  authenticationViewController.h
+//  bbcrt
+//
+//  Created by repinteractive on 17/04/12.
+//  Copyright (c) 2012 momo_dingua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface authenticationViewController : UIViewController
+
+@end
---
+++ bbcrt/mainViewController.h
@@ -0,0 +1,13 @@
+//
+//  mainViewController.h
+//  bbcrt
+//
+//  Created by repinteractive on 17/04/12.
+//  Copyright (c) 2012 momo_dingua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainViewController : UIViewController
+
+@end
